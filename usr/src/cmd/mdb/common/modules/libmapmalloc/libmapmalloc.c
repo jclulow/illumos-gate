9d5ea83356fe604f535b7410dbb6889a00996c17
Joshua M. Clulow <jmc@joyent.com>
2015-01-08 21:52:20 +0000

    XXX debuggable libmapmalloc

---
+++ usr/src/cmd/mdb/common/modules/libmapmalloc/libmapmalloc.c
@@ -0,0 +1,141 @@
+
+#include <mdb/mdb_modapi.h>
+#include <stddef.h>
+
+#include "libmapmalloc_impl.h"
+
+static int
+mdb_mapmalloc_walk_pages_init(mdb_walk_state_t *wsp)
+{
+	GElf_Sym sym;
+	uintptr_t memstart;
+
+	if (wsp->walk_addr != NULL)
+		return (WALK_NEXT);
+
+	/*
+	 * This is a global walk, so lookup the "memstart" symbol as
+	 * the first `struct page` to walk:
+	 */
+	if (mdb_lookup_by_name("mapmalloc_memstart", &sym) != 0) {
+		mdb_warn("could not find symbol 'memstart'");
+		return (WALK_ERR);
+	}
+
+	if (mdb_vread(&memstart, sizeof (memstart), sym.st_value) == -1) {
+		mdb_warn("could not load 'memstart'");
+		return (WALK_ERR);
+	}
+
+	wsp->walk_addr = memstart;
+
+	return (WALK_NEXT);
+}
+
+static int
+mdb_mapmalloc_walk_pages_step(mdb_walk_state_t *wsp)
+{
+	uintptr_t addr;
+	int rv;
+	mapmalloc_page_t page;
+
+	addr = wsp->walk_addr;
+
+	/*
+	 * Pass this "struct page" address out:
+	 */
+	if ((rv = wsp->walk_callback(wsp->walk_addr, NULL,
+	    wsp->walk_cbdata)) != WALK_NEXT) {
+		return (rv);
+	}
+
+	/*
+	 * Read in the struct, so that we can follow the pointer chain:
+	 */
+	if (mdb_vread(&page, sizeof (page), addr) == -1) {
+		mdb_warn("could not read mapmalloc_page_t (%p)", addr);
+		return (WALK_ERR);
+	}
+
+	if (page.mmp_next == NULL)
+		return (WALK_DONE);
+	wsp->walk_addr = (uintptr_t) page.mmp_next;
+
+	return (WALK_NEXT);
+}
+
+static int
+mdb_mapmalloc_walk_blocks_init(mdb_walk_state_t *wsp)
+{
+	if (mdb_layered_walk("mapmalloc_pages", wsp) == -1) {
+		mdb_warn("could not walk 'mapmalloc_pages'");
+		return (WALK_ERR);
+	}
+
+	return (WALK_NEXT);
+}
+
+static int
+mdb_mapmalloc_walk_blocks_step(mdb_walk_state_t *wsp)
+{
+	uintptr_t addr;
+	mapmalloc_page_t page;
+
+	/*
+	 * Load this "struct page":
+	 */
+	addr = wsp->walk_addr;
+	if (mdb_vread(&page, sizeof (page), addr) == -1) {
+		mdb_warn("could not read mapmalloc_page_t (%p)", addr);
+		return (WALK_ERR);
+	}
+
+	/*
+	 * The first block is embedded in this page:
+	 */
+	addr += offsetof(mapmalloc_page_t, mmp_block);
+
+	/*
+	 * Read each "struct block" from this "struct page":
+	 */
+	for (;;) {
+		int rv;
+		mapmalloc_block_t block;
+
+		if ((rv = wsp->walk_callback(addr, NULL, wsp->walk_cbdata)) !=
+		    WALK_NEXT) {
+			return (rv);
+		}
+
+		if (mdb_vread(&block, sizeof (block), addr) == -1) {
+			mdb_warn("could not read mapmalloc_block_t (%p)", addr);
+			return (WALK_ERR);
+		}
+
+		if (block.mmb_next == NULL)
+			return (WALK_NEXT);
+
+		addr = (uintptr_t)block.mmb_next;
+	}
+}
+
+static const mdb_walker_t walkers[] = {
+	{ "mapmalloc_pages", "walk pages in libmapmalloc page list",
+		mdb_mapmalloc_walk_pages_init,
+		mdb_mapmalloc_walk_pages_step },
+	{ "mapmalloc_blocks", "walk blocks in libmapmalloc page list",
+		mdb_mapmalloc_walk_blocks_init,
+		mdb_mapmalloc_walk_blocks_step }
+};
+
+static const mdb_modinfo_t modinfo = {
+	MDB_API_VERSION,
+	NULL,
+	walkers
+};
+
+const mdb_modinfo_t *
+_mdb_init(void)
+{
+	return (&modinfo);
+}
--- usr/src/lib/libmapmalloc/common/textmem.c
+++ usr/src/lib/libmapmalloc/common/textmem.c
@@ -20,17 +20,16 @@
  */
 
 /*
+ * Copyright (c) 2015, Joyent, Inc.
+ */
+
+/*
  * Copyright 2008 Sun Microsystems, Inc.  All rights reserved.
  * Use is subject to license terms.
  */
 
 /*	Copyright (c) 1984, 1986, 1987, 1988, 1989 AT&T	*/
 /*	  All Rights Reserved  	*/
-
-#pragma ident	"%Z%%M%	%I%	%E% SMI"
-
-#include <sys/types.h>
-
 
 /*
  * Simplified version of malloc(), free() and realloc(), to be linked with
@@ -46,6 +45,7 @@
  * New memory chunks are allocated on a first-fit basis.
  * Freed blocks are joined in larger blocks. Free pages are unmapped.
  */
+
 #include <stdlib.h>
 #include <sys/types.h>
 #include <sys/mman.h>
@@ -57,74 +57,94 @@
 #include <synch.h>
 #include <string.h>
 
-static mutex_t lock = DEFAULTMUTEX;
+#include "libmapmalloc_impl.h"
 
-struct block {
-	size_t size;		/* Space available for user */
-	struct page *page;	/* Backwards reference to page */
-	int status;
-	struct block *next;
-	void *memstart[1];
-};
+/*
+ * Unfortunately, assert() appears to allocate memory with malloc(3C).
+ * If we trip an assert, the arena is likely corrupt and _that_ allocation
+ * will trip an assert as well.  Instead, an assertion macro with no
+ * dependencies is included here.
+ */
+char *mapmalloc_panicstr;
+mapmalloc_block_t *mapmalloc_panicblock;
+#define	VERIFY(block, rst)					\
+	do {							\
+		if (!(rst)) {					\
+			mapmalloc_panicblock = (block);		\
+			mapmalloc_panicstr = #rst;		\
+			abort();				\
+		}						\
+	} while (0)
 
-struct page {
-	size_t size;		/* Total page size (incl. header) */
-	struct page *next;
-	struct block block[1];
-};
+#define	VERIFY_MAGIC(block) \
+	VERIFY(block, (block)->mmb_magic == MAPMALLOC_MAGIC)
+#define	VERIFY_BUSY(block) \
+	VERIFY(block, (block)->mmb_status == MMBS_BUSY)
+#define	VERIFY_FREE(block) \
+	VERIFY(block, (block)->mmb_status == MMBS_FREE)
 
-#define	FREE	0
-#define	BUSY	1
+static mutex_t mapmalloc_lock = DEFAULTMUTEX;
 
-#define	HDR_BLOCK	(sizeof (struct block) - sizeof (void *))
-#define	HDR_PAGE	(sizeof (struct page) - sizeof (void *))
-#define	MINSZ		sizeof (double)
-
-/* for convenience */
-#ifndef	NULL
-#define	NULL		(0)
-#endif
-
-struct page *memstart;
+mapmalloc_page_t *mapmalloc_memstart;
 static int pagesize;
-static void defrag(struct page *);
-static void split(struct block *,  size_t);
-static void *malloc_unlocked(size_t);
+static void defrag(mapmalloc_page_t *);
+static void split(mapmalloc_block_t *,  size_t);
+static void *malloc_unlocked(size_t, void *);
 static size_t align(size_t, int);
+
+extern void *caller(void);
+extern __thread void *mapmalloc_calloc_caller;
 
 void *
 malloc(size_t size)
 {
+	void *our_caller = caller();
 	void *retval;
-	(void) mutex_lock(&lock);
-	retval = malloc_unlocked(size);
-	(void) mutex_unlock(&lock);
+
+	/*
+	 * Our calloc() implementation will store its caller in this
+	 * thread-local storage for us to use here.  Without this,
+	 * many allocations would have mmb_pc set to calloc().
+	 */
+	if (mapmalloc_calloc_caller != NULL)
+		our_caller = mapmalloc_calloc_caller;
+
+	(void) mutex_lock(&mapmalloc_lock);
+	retval = malloc_unlocked(size, our_caller);
+	(void) mutex_unlock(&mapmalloc_lock);
+
 	return (retval);
 }
 
-
 static void *
-malloc_unlocked(size_t size)
+malloc_unlocked(size_t size, void *our_caller)
 {
-	struct block *block;
-	struct page *page;
+	mapmalloc_block_t *block;
+	mapmalloc_page_t *page;
 
-	if (pagesize == 0)
+	if (pagesize == 0) {
 		pagesize = (int)sysconf(_SC_PAGESIZE);
+	}
 
 	size = align(size, MINSZ);
 
 	/*
 	 * Try to locate necessary space
 	 */
-	for (page = memstart; page; page = page->next) {
-		for (block = page->block; block; block = block->next) {
-			if (block->status == FREE && block->size >= size)
+	for (page = mapmalloc_memstart; page != NULL; page =
+	    page->mmp_next) {
+		for (block = page->mmp_block; block != NULL; block =
+		    block->mmb_next) {
+			VERIFY_MAGIC(block);
+
+			if (block->mmb_status == MMBS_FREE &&
+			    block->mmb_size >= size) {
 				goto found;
+			}
 		}
 	}
-found:
 
+found:
 	/*
 	 * Need to allocate a new page
 	 */
@@ -132,81 +152,98 @@ found:
 		size_t totsize = size + HDR_PAGE;
 		size_t totpage = align(totsize, pagesize);
 
-		if ((page = (struct page *)mmap(0, totpage,
-		    PROT_READ|PROT_WRITE, MAP_ANON | MAP_PRIVATE, -1, 0))
-		    == MAP_FAILED)
+		if ((page = (mapmalloc_page_t *)mmap(0, totpage,
+		    PROT_READ | PROT_WRITE, MAP_ANON | MAP_PRIVATE, -1,
+		    0)) == MAP_FAILED) {
 			return (0);
+		}
 
-		page->next = memstart;
-		memstart = page;
-		page->size = totpage;
-		block = page->block;
-		block->next = 0;
-		block->status = FREE;
-		block->size = totpage - HDR_PAGE;
-		block->page = page;
+		page->mmp_next = mapmalloc_memstart;
+		mapmalloc_memstart = page;
+		page->mmp_size = totpage;
+		block = page->mmp_block;
+		block->mmb_magic = MAPMALLOC_MAGIC;
+		block->mmb_next = 0;
+		block->mmb_status = MMBS_FREE;
+		block->mmb_size = totpage - HDR_PAGE;
+		block->mmb_page = page;
 	}
 
 	split(block, size);
 
-	block->status = BUSY;
-	return (&block->memstart);
+	VERIFY_MAGIC(block);
+	VERIFY_FREE(block);
+	block->mmb_status = MMBS_BUSY;
+	block->mmb_pc = our_caller;
+	return (&block->mmb_memstart);
 }
 
 void *
 realloc(void *ptr, size_t size)
 {
-	struct block *block;
+	void *our_caller = caller();
+	mapmalloc_block_t *block;
 	size_t osize;
 	void *newptr;
 
-	(void) mutex_lock(&lock);
+	(void) mutex_lock(&mapmalloc_lock);
 	if (ptr == NULL) {
-		newptr = malloc_unlocked(size);
-		(void) mutex_unlock(&lock);
+		newptr = malloc_unlocked(size, our_caller);
+		(void) mutex_unlock(&mapmalloc_lock);
 		return (newptr);
 	}
-	block = (struct block *)((char *)ptr - HDR_BLOCK);
+	block = (mapmalloc_block_t *)((char *)ptr - HDR_BLOCK);
+	VERIFY_MAGIC(block);
+	VERIFY_BUSY(block);
 	size = align(size, MINSZ);
-	osize = block->size;
+	osize = block->mmb_size;
 
 	/*
 	 * Join block with next one if it is free
 	 */
-	if (block->next && block->next->status == FREE) {
-		block->size += block->next->size + HDR_BLOCK;
-		block->next = block->next->next;
+	if (block->mmb_next != NULL && block->mmb_next->mmb_status ==
+	    MMBS_FREE) {
+		block->mmb_size += block->mmb_next->mmb_size + HDR_BLOCK;
+		block->mmb_next = block->mmb_next->mmb_next;
 	}
 
-	if (size <= block->size) {
+	if (size <= block->mmb_size) {
 		split(block, size);
-		(void) mutex_unlock(&lock);
+		(void) mutex_unlock(&mapmalloc_lock);
 		return (ptr);
 	}
 
-	newptr = malloc_unlocked(size);
+	newptr = malloc_unlocked(size, our_caller);
 	(void) memcpy(newptr, ptr, osize);
-	block->status = FREE;
-	defrag(block->page);
-	(void) mutex_unlock(&lock);
+	block->mmb_status = MMBS_FREE;
+	defrag(block->mmb_page);
+	(void) mutex_unlock(&mapmalloc_lock);
 	return (newptr);
 }
 
 void
 free(void *ptr)
 {
-	struct block *block;
+	mapmalloc_block_t *block;
 
-	(void) mutex_lock(&lock);
+	(void) mutex_lock(&mapmalloc_lock);
 	if (ptr == NULL) {
-		(void) mutex_unlock(&lock);
+		(void) mutex_unlock(&mapmalloc_lock);
 		return;
 	}
-	block = (struct block *)((char *)ptr - HDR_BLOCK);
-	block->status = FREE;
 
-	defrag(block->page);
-	(void) mutex_unlock(&lock);
+	block = (mapmalloc_block_t *)((char *)ptr - HDR_BLOCK);
+	VERIFY_MAGIC(block);
+
+	/*
+	 * Check that this is not a double free, then mark the block
+	 * freed:
+	 */
+	VERIFY_BUSY(block);
+	block->mmb_status = MMBS_FREE;
+
+	defrag(block->mmb_page);
+	(void) mutex_unlock(&mapmalloc_lock);
 }
 
 /*
@@ -215,24 +252,29 @@ free(void *ptr)
 static size_t
 align(size_t size, int bound)
 {
-	if (size < bound)
+	if (size < bound) {
 		return ((size_t)bound);
-	else
+	} else {
 		return (size + bound - 1 - (size + bound - 1) % bound);
+	}
 }
 
 static void
-split(struct block *block, size_t size)
+split(mapmalloc_block_t *block, size_t size)
 {
-	if (block->size > size + sizeof (struct block)) {
-		struct block *newblock;
-		newblock = (struct block *)((char *)block + HDR_BLOCK + size);
-		newblock->next = block->next;
-		block->next = newblock;
-		newblock->status = FREE;
-		newblock->page = block->page;
-		newblock->size = block->size - size - HDR_BLOCK;
-		block->size = size;
+	VERIFY_MAGIC(block);
+
+	if (block->mmb_size > size + sizeof (mapmalloc_block_t)) {
+		mapmalloc_block_t *newblock;
+		newblock = (mapmalloc_block_t *)((char *)block + HDR_BLOCK +
+		    size);
+		newblock->mmb_magic = MAPMALLOC_MAGIC;
+		newblock->mmb_next = block->mmb_next;
+		block->mmb_next = newblock;
+		newblock->mmb_status = MMBS_FREE;
+		newblock->mmb_page = block->mmb_page;
+		newblock->mmb_size = block->mmb_size - size - HDR_BLOCK;
+		block->mmb_size = size;
 	}
 }
 
@@ -240,52 +282,58 @@ split(struct block *block, size_t size)
  * Defragmentation
  */
 static void
-defrag(struct page *page)
+defrag(mapmalloc_page_t *page)
 {
-	struct block *block;
+	mapmalloc_block_t *block;
 
-	for (block = page->block; block; block = block->next) {
-		struct block *block2;
+	for (block = page->mmp_block; block != NULL; block = block->mmb_next) {
+		mapmalloc_block_t *block2;
 
-		if (block->status == BUSY)
+		VERIFY_MAGIC(block);
+
+		if (block->mmb_status == MMBS_BUSY) {
 			continue;
-		for (block2 = block->next; block2 && block2->status == FREE;
-		    block2 = block2->next) {
-			block->next = block2->next;
-			block->size += block2->size + HDR_BLOCK;
+		}
+		for (block2 = block->mmb_next; block2 != NULL &&
+		    block2->mmb_status == MMBS_FREE;
+		    block2 = block2->mmb_next) {
+			VERIFY_MAGIC(block2);
+
+			block->mmb_next = block2->mmb_next;
+			block->mmb_size += block2->mmb_size + HDR_BLOCK;
 		}
 	}
 
 	/*
 	 * Free page
 	 */
-	if (page->block->size == page->size - HDR_PAGE) {
-		if (page == memstart)
-			memstart = page->next;
-		else {
-			struct page *page2;
-			for (page2 = memstart; page2->next;
-			    page2 = page2->next) {
-				if (page2->next == page) {
-					page2->next = page->next;
+	if (page->mmp_block->mmb_size == page->mmp_size - HDR_PAGE) {
+		if (page == mapmalloc_memstart) {
+			mapmalloc_memstart = page->mmp_next;
+		} else {
+			mapmalloc_page_t *page2;
+			for (page2 = mapmalloc_memstart; page2->mmp_next !=
+			    NULL; page2 = page2->mmp_next) {
+				if (page2->mmp_next == page) {
+					page2->mmp_next = page->mmp_next;
 					break;
 				}
 			}
 		}
-		(void) munmap((caddr_t)page, page->size);
+		(void) munmap((caddr_t)page, page->mmp_size);
 	}
 }
 
 static void
 malloc_prepare()
 {
-	(void) mutex_lock(&lock);
+	(void) mutex_lock(&mapmalloc_lock);
 }
 
 static void
 malloc_release()
 {
-	(void) mutex_unlock(&lock);
+	(void) mutex_unlock(&mapmalloc_lock);
 }
 
 #pragma init(malloc_init)
