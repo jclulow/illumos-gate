da27f2f5237fe35efc9f9f7a398b62f685e05cab
Joshua M. Clulow <jmc@joyent.com>
2015-02-20 17:14:10 -0800

    OS-3561 lxbrand emulation library should execute on alternate stack OS-3558 lxbrand add support for full in-kernel syscall handling

--- usr/src/lib/brand/lx/lx_brand/common/lx_provider.d
+++ usr/src/lib/brand/lx/lx_brand/common/lx_provider.d
@@ -10,14 +10,26 @@
  */
 
 /*
- * Copyright (c) 2014 Joyent, Inc.  All rights reserved.
+ * Copyright 2015 Joyent, Inc.
  */
 
 provider lx {
 	probe debug(char *buf);
-	probe sigdeliver(int sig, void *lx_sigaction, void *lx_sigstack,
-	    void *lx_ucontext);
+	probe sigdeliver(int sig, void *lx_sigaction, void *lx_sigstack);
 	probe sigreturn(void *lx_ucontext, void *ucontext, uintptr_t sp);
+
+	probe signal_delivery_frame_create(void *lx_sigdeliver_frame);
+	probe signal_delivery_frame_found(void *lx_sigdeliver_frame);
+	probe signal_delivery_frame_corrupt(void *lx_sigdeliver_frame);
+
+	probe signal_post_handler(uintptr_t old_sp, uintptr_t new_sp);
+
+	probe signal_altstack_enable(uintptr_t alt_sp);
+	probe signal_altstack_disable();
+
+	probe emulate_enter(void *ucp, int syscall_num, uintptr_t *args);
+	probe emulate_return(void *ucp, int syscall_num, uintptr_t ret,
+	    uintptr_t errn);
 };
 
 #pragma D attributes Evolving/Evolving/ISA provider lx provider
